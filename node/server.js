69ddf0d05b02c64a9eed8921271cd154f4cd46d0
west0706 <west0706@gmail.com>
2018-06-01 12:56:51 +0000

    test

--- node/server.js
+++ node/server.js
@@ -11,7 +11,7 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
   res.send("<html><head><h1>hostname : " + hostname + 
-	"</h1></head><body><img src='https://cdn-images-1.medium.com/max/1600/0*Hl-1BN46JLiTh8UM.png' height='512' width='512'></body></html>");
+	"</h1></head><body><img src='https://blog.docker.com/wp-content/uploads/Swarmnado-357x627-30-1.gif' height='512' width='512'></body></html>");
 });
 
 app.listen(PORT, HOST);
